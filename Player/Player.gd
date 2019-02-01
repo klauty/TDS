21daa23bef8208ae4ad0078e7c0aa789c20773b0
nao <kaue.developer@gmail.com>
2019-02-01 16:48:50 -0200

    start !

---
+++ Player/Player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+const MOVE_SPEED = 130
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	var move_vec = Vector2()
+	if Input.is_action_pressed("move_up"):
+		move_vec.y -= 1
+	if Input.is_action_pressed("move_down"):
+		move_vec.y += 1
+	if Input.is_action_pressed("move_left"):
+		move_vec.x -= 1
+	if Input.is_action_pressed("move_right"):
+		move_vec.x += 1
+	move_vec = move_vec.normalized()
+	move_and_collide(move_vec * MOVE_SPEED * delta)
+
+func kill():
+	get_tree().reload_current_scene()
